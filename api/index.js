3357655e9957deea6cdc068276f1ed9a686e0eca
JatinBhargava <jatinbhargava4@gmail.com>
2023-06-15 11:01:12 +0530

    trycatch

--- api/index.js
+++ api/index.js
@@ -3,36 +3,40 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const User = require("./model/User");
 var bodyParser = require("body-parser");
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const connectDB = async() =>{
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.URI);
-    console.log('Connect')
+    console.log("Connect");
   } catch (err) {
-    console.log('Failed to connect the DB',err)
+    console.log("Failed to connect the DB", err);
   }
-}
+};
 
-connectDB()
+connectDB();
 
 app.post("/addaccount", async (req, res) => {
-  const { _id, username, email, amount, account_number } = req.body;
-  const UserDoc = await User.create({
-    _id,
-    username,
-    email,
-    amount,
-    account_number,
-  });
+  try {
+    const { _id, username, email, amount, account_number } = req.body;
+    const UserDoc = await User.create({
+      _id,
+      username,
+      email,
+      amount,
+      account_number,
+    });
 
-  res.json(UserDoc);
+    res.json(UserDoc);
+  } catch (error) {
+    console.log("error in posting or adding user", error);
+  }
 });
 
 app.get("/accounts", async (req, res) => {
@@ -45,11 +49,16 @@ app.post("/transcation", async (req, res) => {
   const Sender = await User.find({ account_number: From });
   const Recever = await User.find({ account_number: To });
 
-  const newSender = await User.findOneAndUpdate({account_number : From}, { $inc : { "amount": Amount } })
-  const newRecever = await User.findOneAndUpdate({account_number : To}, { $inc : { "amount" : -Amount } }) 
+  const newSender = await User.findOneAndUpdate(
+    { account_number: From },
+    { $inc: { amount: Amount } }
+  );
+  const newRecever = await User.findOneAndUpdate(
+    { account_number: To },
+    { $inc: { amount: -Amount } }
+  );
 
-  res.json(newSender.amount)
-  
+  res.json(newSender.amount);
 });
 
 app.listen(4000, (req, res) => {
